3a5df43d29b3284515f4150ea3c89050dee22f3c
h3athen <gurungaryan73@gmail.com>
2023-12-30 02:35:51 +0530

    add channel stream

--- main.go
+++ main.go
@@ -8,18 +8,6 @@ import (
 	"time"
 )
 
-// // Behavior-based anomaly detection
-// func detectAnomalies() {
-// }
-
-// // Integration with threat intelligence feeds
-// func integrateThreatIntelligence() {
-// }
-
-// // Extensible and customizable rule engine
-// func applyRules() {
-// }
-
 var (
 	help   *bool
 	net    *bool
@@ -43,6 +31,9 @@ func init() {
 }
 
 func main() {
+
+	messageChan := make(chan string)
+
 	flag.Parse()
 
 	if *help {
@@ -51,26 +42,26 @@ func main() {
 	}
 
 	if *net {
-		modules.NetworkEvents()
+		messageChan <- modules.NetworkEvents()
 		os.Exit(0)
 	}
 
 	if *event {
-		modules.LogEvents()
+		messageChan <- modules.LogEvents()
 		os.Exit(0)
 	}
 
 	if *cpu {
-		modules.MonitorCPU()
+		messageChan <- modules.MonitorCPU()
 	}
 
 	if *mem {
-		modules.MonitorMemory()
+		messageChan <- modules.MonitorMemory()
 	}
 
 	if *fsm {
 		if *path != "." {
-			modules.MonitorFileSystem(*path)
+			messageChan <- modules.MonitorFileSystem(*path)
 		} else {
 			fmt.Println("[-]: Path is not set")
 		}
@@ -78,35 +69,12 @@ func main() {
 
 	if *threat {
 		fmt.Println("[+] Checking for DLL injection...")
-		modules.CheckDllInjection()
+		messageChan <- modules.CheckDllInjection()
 		time.Sleep(2 * time.Second)
 		fmt.Println("[+] Checking for process injection...")
-		modules.CheckProcessInjection()
+		messageChan <- modules.CheckProcessInjection()
 	}
 
-	// go detectAnomalies()
-
-	// go integrateThreatIntelligence()
-
-	// go applyRules()
-
-	// waitForTerminationSignal()
+	message := <-messageChan
+	fmt.Println(message)
 }
-
-// func waitForTerminationSignal() {
-// 	signalChannel := make(chan os.Signal, 1)
-// 	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
-
-// 	<-signalChannel
-
-// 	cleanup()
-
-// 	os.Exit(0)
-// }
-
-// func cleanup() {
-// 	// Implement cleanup tasks here
-// 	fmt.Println("Performing cleanup tasks...")
-// 	time.Sleep(2 * time.Second)
-// 	fmt.Println("Cleanup tasks completed.")
-// }
